189a19fc2fb7d78f2960776b0a4a39e796dde747
finma <firmanmaulana2403@gmail.com>
2021-11-19 16:46:57 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-k9ors3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-k9ors3)
---
+++ src/App.js
@@ -0,0 +1,220 @@
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+function App() {
+  const [audio, setAudio] = useState(bankTwo);
+  const [display, setDisplay] = useState('');
+
+  return (
+    <div id="drum-machine" className="">
+      <PadBank audio={audio} display={display} setDisplay={setDisplay} />
+      <p id="display" />
+    </div>
+  );
+}
+
+const DrumPad = ({ id, url, keyCode, keyTrigger, setDisplay }) => {
+  const audio = new Audio(url);
+  const key = useKeyPress(keyTrigger.toLowerCase());
+
+  const playSound = () => {
+    audio.currentTime = 0;
+    audio.play();
+    setDisplay(id);
+  };
+
+  useEffect(() => {
+    function handleKeyPress(e) {
+      // console.log(e);
+      e.keyCode === keyCode && playSound();
+    }
+
+    window.addEventListener('keyup', handleKeyPress);
+
+    return () => {
+      window.removeEventListener('keyup', handleKeyPress);
+    };
+  }, []);
+
+  return (
+    <motion.div
+      id={keyCode}
+      className="drum-pad"
+      onClick={playSound}
+      whileTap={{
+        scale: 1.2,
+        borderColor: '#ffc600',
+        boxShadow: '0 0 1rem #ffc600'
+      }}
+    >
+      <audio className="clip" data-key={keyCode} src={audio} id={keyTrigger} />
+      <kbd className="key">{keyTrigger}</kbd>
+    </motion.div>
+  );
+};
+
+const PadBank = ({ audio, display, setDisplay }) => {
+  return (
+    <div className="pad-bank-wrapper">
+      <div className="pad-bank">
+        {audio.map((item, i) => (
+          <DrumPad
+            id={item.id}
+            url={item.url}
+            keyCode={item.keyCode}
+            keyTrigger={item.keyTrigger}
+            key={i}
+            setDisplay={setDisplay}
+          />
+        ))}
+      </div>
+      <div id="display" className="display">
+        {display}
+      </div>
+    </div>
+  );
+};
+
+function useKeyPress(targetKey) {
+  // State for keeping track of whether key is pressed
+  const [keyPressed, setKeyPressed] = useState(false);
+  // If pressed key is our target key then set to true
+  function downHandler({ key }) {
+    if (key === targetKey) {
+      setKeyPressed(true);
+    }
+  }
+  // If released key is our target key then set to false
+  const upHandler = ({ key }) => {
+    if (key === targetKey) {
+      setKeyPressed(false);
+    }
+  };
+  // Add event listeners
+  useEffect(() => {
+    window.addEventListener('keydown', downHandler);
+    window.addEventListener('keyup', upHandler);
+    // Remove event listeners on cleanup
+    return () => {
+      window.removeEventListener('keydown', downHandler);
+      window.removeEventListener('keyup', upHandler);
+    };
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+  return keyPressed ? targetKey : '';
+}
+
+const bankOne = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "Kick-n'-Hat",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  }
+];
+
+const bankTwo = [
+  {
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Chord-1',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3'
+  },
+  {
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Chord-2',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3'
+  },
+  {
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Chord-3',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3'
+  },
+  {
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Shaker',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3'
+  },
+  {
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Open-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3'
+  },
+  {
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Closed-HH',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3'
+  },
+  {
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: 'Punchy-Kick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3'
+  },
+  {
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Side-Stick',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3'
+  },
+  {
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Snare',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3'
+  }
+];
+
+export default App;
